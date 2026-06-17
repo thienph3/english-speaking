965761726cadf0a9efbd4a21d61cc9e0e5f634f2
phthien <phthien@vinamilk.com.vn>
2026-06-18 01:36:17 +0700

    fix(android): set compileSdk 35 for supabase_flutter 2.15 compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.speakeng.app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
